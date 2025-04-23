9655b4105706fca087388b4e4767f14ddd19f724
IntGrah <148660026+IntGrah@users.noreply.github.com>
2025-04-23 21:21:49 +0100

    Simplify

--- ttt.cc
+++ ttt.cc
@@ -1,10 +1,9 @@
 #include <algorithm>
 #include <array>
-#include <cstdio>
-#include <cstdlib>
+#include <chrono>
+#include <iostream>
 #include <random>
 #include <vector>
-#include <chrono>
 
 const size_t BOARD_SIZE = 20;
 const size_t BOARD_SIZE_SQUARED = BOARD_SIZE * BOARD_SIZE;
@@ -13,8 +12,8 @@ const size_t WIN_CONDITION = 10;
 class Board
 {
     using BoardArray = std::array<std::array<bool, BOARD_SIZE>, BOARD_SIZE>;
-    using CountArray = std::array<long long, BOARD_SIZE>;
-    using DiagArray = std::array<long long, 2 * BOARD_SIZE - 1>;
+    using CountArray = std::array<size_t, BOARD_SIZE>;
+    using DiagArray = std::array<size_t, 2 * BOARD_SIZE - 1>;
 
     BoardArray board{};
     CountArray cols{};
@@ -36,8 +35,7 @@ public:
 
     bool check_win(size_t x, size_t y) const
     {
-        long long acc;
-        size_t xa, ya;
+        size_t acc, xa, ya;
 
         // Column
         if (cols[x] >= WIN_CONDITION)
@@ -45,11 +43,11 @@ public:
             acc = 1;
             ya = y + 1;
             while (ya < BOARD_SIZE && board[ya++][x])
-                if (++acc == WIN_CONDITION)
+                if (++acc >= WIN_CONDITION)
                     return true;
             ya = y - 1;
             while (ya < BOARD_SIZE && board[ya--][x])
-                if (++acc == WIN_CONDITION)
+                if (++acc >= WIN_CONDITION)
                     return true;
         }
 
@@ -59,11 +57,11 @@ public:
             acc = 1;
             xa = x + 1;
             while (xa < BOARD_SIZE && board[y][xa++])
-                if (++acc == WIN_CONDITION)
+                if (++acc >= WIN_CONDITION)
                     return true;
             xa = x - 1;
             while (xa < BOARD_SIZE && board[y][xa--])
-                if (++acc == WIN_CONDITION)
+                if (++acc >= WIN_CONDITION)
                     return true;
         }
 
@@ -74,12 +72,12 @@ public:
             xa = x + 1;
             ya = y + 1;
             while (xa < BOARD_SIZE && ya < BOARD_SIZE && board[ya++][xa++])
-                if (++acc == WIN_CONDITION)
+                if (++acc >= WIN_CONDITION)
                     return true;
             xa = x - 1;
             ya = y - 1;
             while (xa < BOARD_SIZE && ya < BOARD_SIZE && board[ya--][xa--])
-                if (++acc == WIN_CONDITION)
+                if (++acc >= WIN_CONDITION)
                     return true;
         }
 
@@ -90,12 +88,12 @@ public:
             xa = x + 1;
             ya = y - 1;
             while (xa < BOARD_SIZE && ya < BOARD_SIZE && board[ya--][xa++])
-                if (++acc == WIN_CONDITION)
+                if (++acc >= WIN_CONDITION)
                     return true;
             xa = x - 1;
             ya = y + 1;
             while (xa < BOARD_SIZE && ya < BOARD_SIZE && board[ya++][xa--])
-                if (++acc == WIN_CONDITION)
+                if (++acc >= WIN_CONDITION)
                     return true;
         }
 
@@ -103,71 +101,62 @@ public:
     }
 };
 
-class Game
+enum class Result
 {
-
-    std::mt19937 rng_{1729};
-
-public:
-    enum class Result
-    {
-        Circle,
-        Cross,
-        Draw
-    };
-
-    Game() = default;
-
-    Result play()
-    {
-        std::vector<size_t> free_cells(BOARD_SIZE_SQUARED);
-        std::iota(free_cells.begin(), free_cells.end(), 0);
-        std::shuffle(free_cells.begin(), free_cells.end(), rng_);
-
-        Board circle, cross;
-
-        for (size_t i = 0; i < BOARD_SIZE_SQUARED; ++i)
-        {
-            size_t x = free_cells[i] % BOARD_SIZE;
-            size_t y = free_cells[i] / BOARD_SIZE;
-
-            if (i % 2 == 0)
-            {
-                circle.update(x, y);
-                if (circle.check_win(x, y))
-                    return Result::Circle;
-            }
-            else
-            {
-                cross.update(x, y);
-                if (cross.check_win(x, y))
-                    return Result::Cross;
-            }
-        }
-
-        return Result::Draw;
-    }
+    Circle,
+    Cross,
+    Draw
 };
+
+Result do_game(std::mt19937 &rng)
+{
+    std::vector<size_t> free_cells(BOARD_SIZE_SQUARED);
+    std::iota(free_cells.begin(), free_cells.end(), 0);
+    std::shuffle(free_cells.begin(), free_cells.end(), rng);
+
+    Board circle, cross;
+
+    for (size_t i = 0; i < BOARD_SIZE_SQUARED; ++i)
+    {
+        size_t x = free_cells[i] % BOARD_SIZE;
+        size_t y = free_cells[i] / BOARD_SIZE;
+
+        if (i % 2 == 0)
+        {
+            circle.update(x, y);
+            if (circle.check_win(x, y))
+                return Result::Circle;
+        }
+        else
+        {
+            cross.update(x, y);
+            if (cross.check_win(x, y))
+                return Result::Cross;
+        }
+    }
+
+    return Result::Draw;
+}
 
 int main()
 {
-    Game game;
-    constexpr size_t n = 10000;
-    size_t o = 0, x = 0, draw = 0;
+    std::mt19937 rng{1729};
+    constexpr int n = 10000;
+    int o = 0, x = 0, draw = 0;
 
     auto start = std::chrono::high_resolution_clock::now();
 
     for (size_t i = 0; i < n; ++i)
     {
-        switch (game.play())
+        switch (do_game(rng))
         {
-        case Game::Result::Circle:
+        case Result::Circle:
             o++;
             break;
-        case Game::Result::Cross:
+        case Result::Cross:
             x++;
             break;
-        case Game::Result::Draw:
+        case Result::Draw:
             draw++;
             break;
         }
@@ -176,7 +165,7 @@ int main()
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
-    printf("O/X/Draw: %zu/%zu/%zu\n", o, x, draw);
-    printf("Time taken: %zu ms\n", duration.count());
+    std::cout << "O/X/Draw: " << o << "/" << x << "/" << draw << std::endl;
+    std::cout << "Time taken: " << duration.count() << " ms" << std::endl;
     return 0;
 }
